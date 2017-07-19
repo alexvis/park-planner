c90f840221d5d7f554245d70c6a797f638223483
Alex Visconde <andres.visconde@gmail.com>
2017-07-19 13:30:33 -0400

    Commit spec files

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,15 @@
-class User <ApplicationRecord
+class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
   validates :state, presence: true
+
+  devise :database_authenticatable, 
+   :registerable,
+   :recoverable,
+   :rememberable,
+   :trackable,
+   :validatable
 
 
 end
--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require 'rails_helper'
 
 feature 'sign up' , %Q{
   As an unathenticated user
@@ -13,7 +13,7 @@ feature 'sign up' , %Q{
     fill_in 'Last Name', with: 'Smith'
     fill_in 'Email', with: 'jon@smith.com'
     fill_in 'State', with: 'MA'
-    fill_in 'Password', with: 'password'
+    fill_in 'user_password', with: 'password'
     fill_in 'Password Confirmation', with: 'password'
     click_button 'Sign Up'
 
@@ -21,7 +21,26 @@ feature 'sign up' , %Q{
     expect(page).to have_content("Sign Out")
   end
 
-  scenario 'required information is not supplied'
+  scenario 'required information is not supplied' do
+    visit root_path
+    click_link 'Sign Up'
+    click_button 'Sign Up'
 
-  scenario 'password confirmation does not match confirmation'
+    expect(page).to have_content("can't be blank")
+    expect(page).to_not have_content("Sign Out")
+
+  end
+
+  scenario 'password confirmation does not match confirmation' do
+    visit root_path
+    click_link 'Sign Up'
+
+    fill_in 'user_password', with: 'password'
+    fill_in 'Password Confirmation', with: 'wahtever'
+
+    click_button 'Sign Up'
+
+    expect(page).to have_content("doesn't match")
+    expect(page).to_not have_content("Sign Out")
+  end
 end
