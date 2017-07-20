5f419263561ebd71d6ddaa19bf7694abb7d34925
JesseChamberlain <chamberlain.jesse@gmail.com>
2017-07-20 12:23:46 -0400

    Added React Image Component

---
+++ react/src/components/Image.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class Image extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+    }
+  }
+
+  render() {
+    return(
+      <p>{this.props.park_image}</p>
+    )
+  }
+}
+}
+
+export default Image
