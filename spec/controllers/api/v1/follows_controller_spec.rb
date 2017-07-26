808f629e3694a8e4e4bc4e8d88ebf7954a79ebaf
Steven Tenaglia <stenten@gmail.com>
2017-07-26 10:45:14 -0400

    Appease the Hound

--- spec/controllers/api/v1/follows_controller_spec.rb
+++ spec/controllers/api/v1/follows_controller_spec.rb
@@ -7,21 +7,21 @@ RSpec.describe Api::V1::FollowsController, type: :controller do
   describe "POST#create" do
     it "creates a new Follow" do
       post_json = {
-	follow: {
-	  park_id: first_park.id,
-	  user_id: first_user.id
-	}
+        follow: {
+          park_id: first_park.id,
+          user_id: first_user.id
+        }
       }.to_json
 
-      expect{ post(:create, body: post_json)}.to change{ Follow.count }.by 1
+      expect{ post(:create, body: post_json) }.to change{ Follow.count }.by 1
     end
 
     it "returns the json with the result" do
       post_json = {
-	follow: {
-	  park_id: first_park.id,
-	  user_id: first_user.id
-	}
+        follow: {
+          park_id: first_park.id,
+          user_id: first_user.id
+        }
       }.to_json
 
       post(:create, body: post_json)
@@ -36,14 +36,14 @@ RSpec.describe Api::V1::FollowsController, type: :controller do
 
     it "deletes an existing Follow" do
       post_json = {
-	follow: {
-	  park_id: first_park.id,
-	  user_id: first_user.id
-	}
+        follow: {
+          park_id: first_park.id,
+          user_id: first_user.id
+        }
       }.to_json
 
-      expect{ post(:create, body: post_json)}.to change{ Follow.count }.by 1
-      expect{ post(:create, body: post_json)}.to change{ Follow.count }.by -1
+      expect{ post(:create, body: post_json) }.to change{ Follow.count }.by 1
+      expect{ post(:create, body: post_json) }.to change{ Follow.count }.by -1
 
       returned_json = JSON.parse(response.body)
       expect(returned_json["following"]).to eq false
@@ -64,10 +64,10 @@ RSpec.describe Api::V1::FollowsController, type: :controller do
 
     it "should return true after an entry has been input" do
       post_json = {
-	follow: {
-	  park_id: first_park.id,
-	  user_id: first_user.id
-	}
+        follow: {
+          park_id: first_park.id,
+          user_id: first_user.id
+        }
       }.to_json
 
       expect{ post(:create, body: post_json)}.to change{ Follow.count }.by 1
