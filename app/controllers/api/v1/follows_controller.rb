cb8917e3fbcfb8e312e123aa194b9fad3d8f699e
Steven Tenaglia <stenten@gmail.com>
2017-07-26 10:32:22 -0400

    Fixed Codeclimate issues in api/v1/follows controller

--- app/controllers/api/v1/follows_controller.rb
+++ app/controllers/api/v1/follows_controller.rb
@@ -2,27 +2,30 @@ class Api::V1::FollowsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
-    if(Follow.find_by park_id: params[:park_id], user_id: params[:user_id])
-      render json: {following: true}
+    if Follow.find_by(park_id: params[:park_id], user_id: params[:user_id])
+      render json: { following: true }
     else
-      render json: {following: false}
+      render json: { following: false }
     end
   end
 
-  def create 
-    # Toggles creating and deleting the following entry in order to follow and unfollow
+  def create
+    # Toggles creating and deleting the following entry
+    # in order to follow and unfollow
     data = JSON.parse(request.body.read)
-    dataFollow = data["follow"]
-    follow = Follow.find_by park_id: dataFollow["park_id"], user_id: dataFollow["user_id"]
+    data_follow = data["follow"]
+    follow = Follow.find_by(
+      park_id: data_follow["park_id"],
+      user_id: data_follow["user_id"])
 
-    if(follow)
+    if follow
       # Follow exists, destroy it
       Follow.destroy(follow.id)
-      render json: {following: false}
-    else 
+      render json: { following: false }
+    else
       # Follow does not exist, create it
-      follow = Follow.create(dataFollow)
-      render json: {following: true}
+      Follow.create(data_follow)
+      render json: { following: true }
     end
   end
 end
