3419f79bc384f4513ee845c4232f76f9483e5cc5
Alex Visconde <andres.visconde@gmail.com>
2017-07-26 09:38:57 -0400

    edit user info from my account page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,17 @@
 class UsersController < ApplicationController
   def index
-
   end
   def show
-    # @user = User.find(params[:id])
   end
   def edit
   end
 
   def create
-    @user = current_user
   end
+
   def update
     @user = current_user
+
     if @user.update(user_params)
       redirect_to user_path(@user)
     else
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   validates :email, presence: true
   validates :state, presence: true
   validates :user_name, presence: true
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create
 
   devise :database_authenticatable,
     :registerable,
