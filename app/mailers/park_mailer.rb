27d9268f094400429886748bd1f2441c75db3312
Steven Tenaglia <stenten@gmail.com>
2017-07-27 12:21:20 -0400

    Created html and text views for new_review

--- app/mailers/park_mailer.rb
+++ app/mailers/park_mailer.rb
@@ -1,2 +1,7 @@
 class ParkMailer < ApplicationMailer
+  def new_review(park, followers)
+    @park = park
+    @followers = followers.map { |f| f.email } 
+    mail(to: @followers, subject: "New Review for #{park.name}")
+  end
 end
