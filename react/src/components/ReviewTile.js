0cef95d3e3ce3cb5b05d62b98771708398252062
Steven Tenaglia <stenten@gmail.com>
2017-07-27 12:55:45 -0400

    Update ReviewTile to display number of upvotes and downvotes. Still need to update these when the button is pushed

--- react/src/components/ReviewTile.js
+++ react/src/components/ReviewTile.js
@@ -6,6 +6,8 @@ const ReviewTile = props => {
     <div>
       <p id="comment">{props.reviewContent.comment}</p>
       <p id="rating">{props.reviewContent.park_rating}</p>
+      <p id="upvotes">{props.reviewContent.num_upvotes}</p>
+      <p id="downvotes">{props.reviewContent.num_downvotes}</p>
       <VoteButton
         reviewId={props.reviewContent.id}
 	      userId={props.reviewContent.user_id}
