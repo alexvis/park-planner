3c31a6950fd1a81a418c78d0de432ee7567054b3
Steven Tenaglia <stenten@gmail.com>
2017-07-27 13:33:03 -0400

    Revise ReviewTile to say Upvotes and Downvotes

--- react/src/components/ReviewTile.js
+++ react/src/components/ReviewTile.js
@@ -29,8 +29,8 @@ class ReviewTile extends React.Component {
       <div>
         <p id="comment">{this.props.reviewContent.comment}</p>
         <p id="rating">{this.props.reviewContent.park_rating}</p>
-        <p id="upvotes">{this.state.numUpvotes}</p>
-        <p id="downvotes">{this.state.numDownvotes</p>
+        <p id="upvotes">Upvotes: {this.state.numUpvotes}</p>
+        <p id="downvotes">Downvotes: {this.state.numDownvotes</p>
         <VoteButton
           reviewId={this.props.reviewContent.id}
           userId={this.props.reviewContent.user_id}
