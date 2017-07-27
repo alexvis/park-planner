00e9550a6a9e7c7e5035be241879687de1e3f567
Steven Tenaglia <stenten@gmail.com>
2017-07-27 13:07:56 -0400

    Send an email to a park's followers when a new review is created

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -13,6 +13,10 @@ class Api::V1::ReviewsController < ApplicationController
     data = JSON.parse(request.body.read)
     review = Review.new(data)
     review.save
+
+    park = Park.find(data.park_id)
+    followers = park.users
+    ParkMailer.new_review(park, followers).deliver_later 
     render json: data
   end
 end
