6f39454aa5cb9d7bbfecb9f7afbf41aa7ee33a6f
JesseChamberlain <chamberlain.jesse@gmail.com>
2017-07-26 22:14:56 -0400

    Finalized Admin functionality

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,16 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :welcome, only: [:index]
   resources :users
-  resources :parks, only: [:show]
+  resources :parks, only: [:show, :destroy]
+  resources :reviews, only: [:destroy]
 
   namespace :api do
     namespace :v1 do
       resources :parks, only: [:show] do
         resources :reviews, only: [:index]
-	resources :users, only: [:show] do
-	  resources :follows, only: [:index]
-	end
+	      resources :users, only: [:show] do
+	         resources :follows, only: [:index]
+	      end
       end
       resources :follows, only: [:create]
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 ParkSeeder.seed!
+
+
+# u1 = User.create(email: 'jessedanger@gmail.com', first_name: 'Jon', last_name: 'Smith', state: 'MA', user_name: 'Jdog', password: 'password', password_confirmation: 'password', profile_photo: '', role: 'admin')
+# u2 = User.create(email: 'chamberlain.jesse@gmail.com', first_name: 'J', last_name: 'Smithy', state: 'MA', user_name: 'Smithy', password: 'password', password_confirmation: 'password', profile_photo: '', role: 'member')
+# r1 = Review.create(comment: "good park", park_rating: 4, park_id: , user_id: )
+# r2 = Review.create(comment: "best park", park_rating: 5, park_id: , user_id: )
+# p1 = Park.create(name: "Yosamite", state: "CA", link_url: "www.park.com")
+# p2 = Park.create(name: "Yellowstone", state: "CA", link_url: "www.YS.com")
