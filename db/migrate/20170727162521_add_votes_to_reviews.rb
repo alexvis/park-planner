91af0543601b4041cea25229d753f7522ae570c9
Steven Tenaglia <stenten@gmail.com>
2017-07-27 12:32:54 -0400

    Set default num votes values to zero

--- db/migrate/20170727162521_add_votes_to_reviews.rb
+++ db/migrate/20170727162521_add_votes_to_reviews.rb
@@ -1,6 +1,6 @@
 class AddVotesToReviews < ActiveRecord::Migration[5.1]
   def change
-    add_column :reviews, :num_upvotes, :integer
-    add_column :reviews, :num_downvotes, :integer
+    add_column :reviews, :num_upvotes, :integer, default: 0
+    add_column :reviews, :num_downvotes, :integer, default: 0
   end
 end
