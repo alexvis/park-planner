a26d71a15ebb6f02aceeaa86e811a3709b040551
JesseChamberlain <chamberlain.jesse@gmail.com>
2017-07-28 06:42:24 -0400

    created seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-ParkSeeder.seed!
+# ParkSeeder.seed!
 #
 # u1 = User.create(email: 'jessedanger@gmail.com', first_name: 'Jon', last_name: 'Smith', state: 'MA', user_name: 'Jdog', password: 'password', password_confirmation: 'password', profile_photo: '', role: 'admin')
 # u2 = User.create(email: 'chamberlain.jesse@gmail.com', first_name: 'J', last_name: 'Smithy', state: 'MA', user_name: 'Smithy', password: 'password', password_confirmation: 'password', profile_photo: '', role: 'member')
@@ -15,73 +15,108 @@ ParkSeeder.seed!
 # p1 = Park.create(name: "Yosamite", state: "CA", link_url: "www.park.com")
 # p2 = Park.create(name: "Yellowstone", state: "CA", link_url: "www.YS.com")
 #
-# USERS = [
-#   {
-#     email: 'jessedanger@gmail.com',
-#     first_name: 'Jon',
-#     last_name: 'Smith',
-#     state: 'MA',
-#     user_name: 'Jdog',
-#     password: 'password',
-#     password_confirmation: 'password',
-#     profile_photo: ''
-#   },
-#   {
-#     email: 'chamberlain.jesse@gmail.com',
-#     first_name: 'Jon',
-#     last_name: 'Smith',
-#     state: 'MA',
-#     user_name: 'Jdanger',
-#     password: 'password',
-#     password_confirmation: 'password',
-#     profile_photo: ''
-#   }
-# ].freeze
-#
-# PARKS = [
-#   {
-#     name: "Yosemite",
-#     state: "CA",
-#     link_url: "https://www.nps.gov/yose/index.htm"
-#   },
-#   {
-#     name: "Adams",
-#     state: "MA",
-#     link_url: "https://www.nps.gov/adam/index.htm"
-#   }
-# ].freeze
-#
-#
-# PARKS.each do |p|
-#   park = Park.find_or_initialize_by(name: p[:name])
-#   park.assign_attributes(p)
-#   park.save!
-# end
-#
-# USERS.each do |p|
-#   user =User.find_or_initialize_by(email: p[:email])
-#   user.assign_attributes(p)
-#   user.save!
-# end
-#
-# REVIEWS = [
-#   {
-#     park_rating: 5.0,
-#     park_id: Park.find_by(name: "Yosemite").id,
-#     user_id: User.find_by(email: 'jessedanger@gmail.com').id
-#   },
-#   {
-#     park_rating: 4.5,
-#     park_id: Park.find_by(name: "Yosemite").id,
-#     user_id: User.find_by(email: 'chamberlain.jesse@gmail.com').id
-#   }
-# ].freeze
-#
-# REVIEWS.each do |park|
-#   review = Review.find_or_initialize_by(park_id: Park.find_by(name: "Yosemite").id)
-#   review.assign_attributes(p)
-#   review.save!
-# end
+USERS = [
+  {
+    email: 'jessedanger@gmail.com',
+    first_name: 'Jesse',
+    last_name: 'Danger',
+    state: 'MA',
+    user_name: 'JDanger',
+    password: 'password',
+    password_confirmation: 'password',
+    profile_photo: '',
+    role: 'admin'
+  },
+  {
+    email: 'chamberlain.jesse@gmail.com',
+    first_name: 'Jesse',
+    last_name: 'Chamberlain',
+    state: 'VT',
+    user_name: 'JChamberlain',
+    password: 'password',
+    password_confirmation: 'password',
+    profile_photo: '',
+    role: 'member'
+  },
+  {
+    email: 'test@gmail.com',
+    first_name: 'Bill',
+    last_name: 'Watterson',
+    state: 'VT',
+    user_name: 'CandH',
+    password: 'password',
+    password_confirmation: 'password',
+    profile_photo: '',
+    role: 'member'
+  }
+].freeze
+
+PARKS = [
+  {
+    name: "Yosemite",
+    description: "It's pretty famous",
+    state: "CA",
+    link_url: "https://www.nps.gov/yose/index.htm"
+  },
+  {
+    name: "Adams",
+    description: "It's Adams park",
+    state: "MA",
+    link_url: "https://www.nps.gov/adam/index.htm"
+  }
+].freeze
+
+
+PARKS.each do |p|
+  park = Park.find_or_initialize_by(name: p[:name])
+  park.assign_attributes(p)
+  park.save!
+end
+
+USERS.each do |p|
+  user =User.find_or_initialize_by(email: p[:email])
+  user.assign_attributes(p)
+  user.save!
+end
+
+REVIEWS = [
+  {
+    park_rating: 5,
+    park_id: Park.find_by(name: "Yosemite").id,
+    user_id: User.find_by(email: 'jessedanger@gmail.com').id,
+    dog_friendly_rating: 5,
+    playground_rating: 5,
+    camping_rating: 5,
+    hiking_rating: 5,
+    scenery_rating: 5
+  },
+  {
+    park_rating: 4,
+    park_id: Park.find_by(name: "Yosemite").id,
+    user_id: User.find_by(email: 'chamberlain.jesse@gmail.com').id,
+    dog_friendly_rating: 4,
+    playground_rating: 4,
+    camping_rating: 4,
+    hiking_rating: 4,
+    scenery_rating: 4
+  },
+  {
+    park_rating: 3,
+    park_id: Park.find_by(name: "Yosemite").id,
+    user_id: User.find_by(email: 'test@gmail.com').id,
+    dog_friendly_rating: 2,
+    playground_rating: 4,
+    camping_rating: 1,
+    hiking_rating: 4,
+    scenery_rating: 3
+  }
+].freeze
+
+REVIEWS.each do |park|
+  review = Review.find_or_initialize_by(user_id: User.find_by(id: park[:user_id]).id)
+  review.assign_attributes(park)
+  review.save!
+end
 
 # STATES.each do |state_info|
 #   abb, full_name = state_info
